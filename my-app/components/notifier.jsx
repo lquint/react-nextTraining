c2890923caaca1fb0a930d1ab7b2dc8800453c72
lquint <83752971+lquint@users.noreply.github.com>
2023-01-21 23:57:39 +0100

    formated

--- my-app/components/notifier.jsx
+++ my-app/components/notifier.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 
 const Notifier = () => {
-    const [count,increment] = useState(0)
-    const handleClick= (e) => {
-        e.preventDefault()
-        increment((c)=>c+1)
-    }
+  const [count, increment] = useState(0);
+  const handleClick = (e) => {
+    e.preventDefault();
+    increment((c) => c + 1);
+  };
 
-    useEffect(()=>{
-        document.title = `${count>0 ? "("+count+")" : ""} Test Page`
-    })
+  useEffect(() => {
+    document.title = `${count > 0 ? "(" + count + ")" : ""} Test Page`;
+  });
 
-    return <button onClick={handleClick} className="bg-blue-400 rounded-md mx-4">Add a notification</button>;
-}
- 
+  return (
+    <button onClick={handleClick} className="bg-blue-400 rounded-md mx-4">
+      Add a notification
+    </button>
+  );
+};
+
 export default Notifier;
--- my-app/pages/testPage.jsx
+++ my-app/pages/testPage.jsx
@@ -3,26 +3,25 @@ import GenericCounter from "@/components/genericCounter";
 import Notifier from "@/components/notifier";
 import SkillSet from "@/components/skillSet";
 
-export default function TestPage(){
-    return (
-        <div>
-            <div>
-                <p>Simple counter</p>
-                <Counter className=""></Counter>
-            </div>
-            <div>
-                <p>Generic counters</p>
-                <GenericCounter initial={0} step={2}></GenericCounter>
-                <GenericCounter initial={0} step={10}></GenericCounter>
-                <GenericCounter initial={100} step={-1}></GenericCounter>
-            </div>
-            <div>
-                <SkillSet></SkillSet>
-            </div>
-            <div>
-                <Notifier></Notifier>
-            </div>
-            
-        </div>
-    )
+export default function TestPage() {
+  return (
+    <div>
+      <div>
+        <p>Simple counter</p>
+        <Counter className=""></Counter>
+      </div>
+      <div>
+        <p>Generic counters</p>
+        <GenericCounter initial={0} step={2}></GenericCounter>
+        <GenericCounter initial={0} step={10}></GenericCounter>
+        <GenericCounter initial={100} step={-1}></GenericCounter>
+      </div>
+      <div>
+        <SkillSet></SkillSet>
+      </div>
+      <div>
+        <Notifier></Notifier>
+      </div>
+    </div>
+  );
 }
