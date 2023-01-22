30c8beaff2eaa7bb2db148e94cc01a430fc13acd
lquint <83752971+lquint@users.noreply.github.com>
2023-01-22 23:21:54 +0100

    formated components

--- my-app/components/toDoList.jsx
+++ my-app/components/toDoList.jsx
@@ -1,35 +1,38 @@
 import { useEffect, useState } from "react";
 
 const ToDoList = () => {
-    const [todos,setTodos]=useState([])
-    const [loading,setLoading]=useState(true)
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect( function(){
-        (async function(){
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-            const responseData = await response.json()
-            console.log(responseData)
-            if ( response.ok){
-                setTodos(responseData)
-            } else{
-                alert(JSON.stringify(responseData))
-            }
-            setLoading(false)
-        })()
-    },[])
-    return ( 
-        <div className="m-2">
-        <p className="font-semibold">TODO List</p>
-        {loading ? 
+  useEffect(function () {
+    (async function () {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      if (response.ok) {
+        setTodos(responseData);
+      } else {
+        alert(JSON.stringify(responseData));
+      }
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <div className="m-2">
+      <p className="font-semibold">TODO List</p>
+      {loading ? (
         <p>Loading list ...</p>
-        :
+      ) : (
         <ul className="ml-8 list-decimal">
-        {todos.map(t => <li>{t.title}</li>)}
+          {todos.map((t) => (
+            <li>{t.title}</li>
+          ))}
         </ul>
-        }
-        
-        </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default ToDoList;
--- my-app/components/toggleCounter.jsx
+++ my-app/components/toggleCounter.jsx
@@ -2,27 +2,27 @@ import Counter from "./counter";
 import { useEffect, useState } from "react";
 
 const ToggleCounter = () => {
-    const [visibleCounter,toggleCounter] = useToggle(true)
+  const [visibleCounter, toggleCounter] = useToggle(true);
 
-    function useToggle(initialValue=true){
-        
-        const [value,setValue]=useState(initialValue)
-        const toggler = ()=>{
-            setValue(boolValue => !boolValue)
-        }
-        
-        return [
-            value,
-            toggler
-        ]
-    }
+  function useToggle(initialValue = true) {
+    const [value, setValue] = useState(initialValue);
+    const toggler = () => {
+      setValue((boolValue) => !boolValue);
+    };
 
-    return ( 
-        <div className="flex min-h-[64px]">
-            <input className="p-1 mx-4 my-auto" type="checkbox" onChange={toggleCounter}/>
-            {visibleCounter && <Counter></Counter>}
-        </div>
-     );
-}
- 
+    return [value, toggler];
+  }
+
+  return (
+    <div className="flex min-h-[64px]">
+      <input
+        className="p-1 mx-4 my-auto"
+        type="checkbox"
+        onChange={toggleCounter}
+      />
+      {visibleCounter && <Counter></Counter>}
+    </div>
+  );
+};
+
 export default ToggleCounter;
